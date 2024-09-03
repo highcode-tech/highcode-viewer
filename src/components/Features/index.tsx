287eeafee8bed08ec0bf597b83f9192a386e0a7e
eugene.park0127 <eugene.park0127@gmail.com>
2024-09-03 10:56:47 -0400

    [push] test eugene

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -8,14 +8,17 @@ const Features = () => {
       <section id="features" className="py-16 md:py-20 lg:py-28">
         <div className="container">
           <SectionTitle
-            title="Main Features"
+            title="Tech Features"
             paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
             center
           />
+          <div className="feature_type">
+
+          </div>
 
           <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
             {featuresData.map((feature) => (
-              <SingleFeature key={feature.id} feature={feature} />
+              <SingleFeature key={feature.id} feature={feature} />   
             ))}
           </div>
         </div>
