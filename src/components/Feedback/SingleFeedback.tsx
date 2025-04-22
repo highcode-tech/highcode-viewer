76456e8ad1c64dc7b3509d3f08225a2fc225967a
mikebiz-bug <michael.lopez.biz91@gmail.com>
2025-04-21 19:10:08 -0500

    [update] feedback info

--- src/components/Feedback/SingleFeedback.tsx
+++ src/components/Feedback/SingleFeedback.tsx
@@ -30,15 +30,17 @@ const SingleFeedback = ({ Feedback }: { Feedback: Feedback }) => {
           </p>
           <div className="flex items-center">
             <div className="mr-5 flex items-center border-r border-body-color border-opacity-10 pr-5 dark:border-white dark:border-opacity-10 xl:mr-3 xl:pr-3 2xl:mr-5 2xl:pr-5">
-              <div className="mr-4">
+              {/* <div className="mr-4">
                 <div className="relative h-10 w-10 overflow-hidden rounded-full">
                   <img src={author.image} alt="author" />
                 </div>
-              </div>
+              </div> */}
               <div className="w-full">
                 <h4 className="mb-1 text-sm font-medium text-dark dark:text-white">
                   By {author.name}
                 </h4>
+                <p className="text-xs text-body-color">{author.address}</p>
+                <p className="text-xs text-body-color">{author.phone}</p>
                 <p className="text-xs text-body-color">{author.designation}</p>
               </div>
             </div>
